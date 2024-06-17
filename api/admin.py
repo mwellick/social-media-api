e7fd2c18f70bf4ed4236101112e9dbe1cb168ae6
Mykhailo Korotia <mr.anderson528491@gmail.com>
2024-06-17 18:43:27 +0300

    Optimized django admin panel and created superuser

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import (
+    Post,
+    Comment,
+    Like,
+    Follow,
+    Unfollow,
 
-# Register your models here.
+)
+
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Follow)
+admin.site.register(Unfollow)
