23aeb8633f122b8ff1e9fc3b053f44a4b437a411
Mykhailo Korotia <mr.anderson528491@gmail.com>
2024-06-17 18:42:09 +0300

    Created models for api app and user app,made migrations and migrated

--- api/models.py
+++ api/models.py
@@ -1,3 +1,91 @@
 from django.db import models
+from django.conf import settings
+from user.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="posts"
+    )
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    post_media = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return f" {self.author}: {self.content[:20]}..."
+
+
+class Comment(models.Model):
+    comment_author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    post_comment = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return (
+            f"{self.comment_author} left a comment on a "
+            f"{self.post_comment.author} post"
+        )
+
+
+class Like(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="likes"
+    )
+    liked_post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="likes"
+    )
+
+    def __str__(self):
+        return (
+            f"@{self.user.username} liked "
+            f"@{self.liked_post.author.username} post"
+        )
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="following"
+    )
+    following = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="followers"
+    )
+    followed_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.follower.username} followed {self.following.username}"
+
+
+class Unfollow(models.Model):
+    unfollower = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="unfollowing"
+    )
+    unfollowed = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="unfollowers"
+    )
+    unfollowed_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.unfollower.username} unfollowed {self.unfollower.username}"
