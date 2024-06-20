a6eba36d1ae07c4841520d0659e69f3c690f1bdc
Mykhailo Korotia <mr.anderson528491@gmail.com>
2024-06-20 20:13:52 +0300

    Changed custim permission logic

--- api/permissions.py
+++ api/permissions.py
@@ -3,9 +3,13 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAdminAllORIsAuthenticatedOrReadOnly(BasePermission):
 
-    def has_permission(self, request, view):
+    def has_object_permission(self, request, view, obj):
+
         return bool(
-            request.method in SAFE_METHODS
-            and request.user
-            and request.user.is_authenticated
-        ) or (request.user and request.user.is_staff)
+            (
+                request.method in SAFE_METHODS
+                and request.user
+                and request.user.is_authenticated
+            )
+            or obj.author == request.user
+        )
